b6ce01090103029ab3cb459149405ae602c324b2
Navya <navyashreec24@gmail.com>
2018-12-08 12:33:30 -0800

    Game logic part added to Game class

--- GameLogic.hpp
+++ GameLogic.hpp
@@ -5,19 +5,36 @@
  *      Author: Vishal
  */
 
-#ifndef GAME_LOGIC_HPP
-#define GAME_LOGIC_HPP
+#ifndef GAME_HPP
+#define GAME_HPP
 
 #include "DisplayApp.hpp"
 
-class GameLogic {
+enum Directions {
+    Still,
+    Up,
+    Down,
+    Right,
+    Left
+};
+
+class Game {
     public:
         int grid[4][4];
+        int score;
         DisplayApp *displayApp;
 
     public:
-        GameLogic(DisplayApp *displayApp);
+        Game(DisplayApp *displayApp);
         void updateGrid();
+        void run();
+        void moveTiles(Directions inputDirection);
+        void rotate();
+        void generate();
+        void moveUp();
+        void moveDown();
+        void moveLeft();
+        void moveRight();
 };
 
 #endif
